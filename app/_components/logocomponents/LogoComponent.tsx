0aef6cc2171eb62f9b186315ad1f0ba00a73bb46
shau8122 <shau8122@kgpian.iitkgp.ac.in>
2023-12-12 18:30:40 +0530

    font updated

--- app/_components/logocomponents/LogoComponent.tsx
+++ app/_components/logocomponents/LogoComponent.tsx
@@ -6,14 +6,14 @@ const LogoComponent = () => {
   const slides = ["/bottle1.jpg", "/bottle2.jpg", "/bottle3.jpg"];
   return (
     <div className=" md:h-auto mt-64 lg:mt-0 w-full max-w-screen-2xl mx-auto bg-[#b9e2f5]/75">
-        <div className="max-w-[1080px] w-full mx-auto ">
+        <div className="max-w-[1080px]  w-full mx-auto ">
           <AnimatedText
-            text="we create emotion for your  promotion"
-            className="mt-10 text-2xl  sm:text-3xl md:text-4xl lg:text-5xl  !text-center font-semibold text-[#0084CB]"
+            text="we create emotion for your  promotion,"
+            className=" !text-center font-semibold text-[#0084CB]"
           />
           <AnimatedText
-            text="Connect your brand to consumers Hand"
-            className=" text-2xl sm:text-3xl md:text-4xl lg:text-5xl  !text-center font-semibold text-[#0084CB]"
+            text={`"Connect your brand to consumers Hand"`}
+            className=" !text-center font-semibold text-[#0084CB]"
           />
         </div>
       <div className="grid grid-cols-12 justify-between items-center gap-2 w-full h-auto md:h-full">
