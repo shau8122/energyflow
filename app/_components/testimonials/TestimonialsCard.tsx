41bd0b9e16f4dd34b3b1cfb7752ae2ce1736e699
shau8122 <shau8122@kgpian.iitkgp.ac.in>
2024-04-14 00:47:24 +0530

    testimonials update

--- app/_components/testimonials/TestimonialsCard.tsx
+++ app/_components/testimonials/TestimonialsCard.tsx
@@ -1,8 +1,19 @@
 import quoteUp from "@/public/quote-up.svg";
 import quoteDown from "@/public/quote-down.svg";
 import Image from "next/image";
+import React from "react";
 
-const TestimonialsCard = () => {
+interface TestimonialsCardProps {
+  name: string;
+  place: string;
+  description: string;
+}
+
+const TestimonialsCard: React.FC<TestimonialsCardProps> = ({
+  name,
+  place,
+  description,
+}) => {
   return (
     <div className="w-full h-full bg-[#dcf0fa]  flex flex-col p-6">
       <div className="md:w-10 md:h-10 w-5 h-5 relative">
@@ -14,14 +25,8 @@ const TestimonialsCard = () => {
         />
       </div>
       <div className="p-6">
-        <p className="text-lg text-slate-700">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-          voluptate, quas, quia, voluptatum quod nemo quos consequatur
-          voluptatibus fugit doloribus voluptas. Quibusdam voluptate, quas,
-          quia, voluptatum quod nemo quos consequatur voluptatibus fugit
-          doloribus voluptas.
-        </p>
-        <h4 className="text-xl font-semibold mt-4">name of autfa-flip-horizontalhor</h4>
+        <p className="text-lg text-slate-700">{description}</p>
+        <h4 className="text-xl font-semibold mt-4">{`${name}(${place})`}</h4>
       </div>
       <div className="w-full flex justify-end ">
         <div className="md:w-10 md:h-10 w-5 h-5  relative">
--- app/_components/testimonials/TestimonialsCarousel.tsx
+++ app/_components/testimonials/TestimonialsCarousel.tsx
@@ -12,6 +12,39 @@ import "swiper/css/pagination";
 import { images } from "@/libs/Image";
 import TestimonialsCard from "./TestimonialsCard";
 
+const testimonials = [
+  {
+    name: "Subroto Layek",
+    place: "Hotel Basundhara",
+    description:
+      "I absolutely love the branded water bottles provided by this hotel! They're always readily available, and the design is sleek and stylish. It's a thoughtful touch that adds to the overall experience of my stay.",
+  },
+  {
+    name: "Sayan Kar",
+    place: "Wonder Inn",
+    description:
+      "Impressed by the quality of service here. The branded water bottles are a nice touch, especially when I'm exploring the city or lounging by the pool. It's refreshing to see a hotel prioritize guest hydration with such attention to detail.",
+  },
+  {
+    name: "Pragya Shukla",
+    place: "Hotel Relook",
+    description:
+      "This hotel's commitment to sustainability shines through with their use of branded water bottles. I appreciate their eco-friendly approach while still providing a convenient amenity for guests. It's a win-win!",
+  },
+  {
+    name: "Shivani Saha",
+    place: "Hotel Prince",
+    description:
+      "The complimentary branded water bottles are a standout feature of this hotel. It's a small gesture that goes a long way in enhancing the guest experience. Plus, the bottles are durable and perfect for taking on outings during my stay.",
+  },
+  {
+    name: "Kamal Adani",
+    place: "Tribit Kitchen",
+    description:
+      "The branded water bottles are a delightful surprise! They're always stocked in my room, making it easy to stay hydrated while exploring the city. It's the little touches like this that set this hotel apart from the rest.",
+  },
+];
+
 const TestimonialsCarousel = () => {
   return (
     <Swiper
@@ -19,15 +52,14 @@ const TestimonialsCarousel = () => {
       spaceBetween={10}
       slidesPerView={1}
       breakpoints={{
-        0:{
-          slidesPerView:1
+        0: {
+          slidesPerView: 1,
         },
         768: {
           slidesPerView: 2,
           spaceBetween: 20,
         },
       }}
-     
       loop={true} // Enable the infinite loop
       autoplay={{ delay: 3000, disableOnInteraction: false }} // Autoplay settings
       modules={[Navigation, Autoplay]}
@@ -35,24 +67,15 @@ const TestimonialsCarousel = () => {
       scrollbar={{ draggable: true }}
       className="w-full rounded-lg"
     >
-      <SwiperSlide>
-        <TestimonialsCard />
-      </SwiperSlide>
-      <SwiperSlide>
-        <TestimonialsCard />
-      </SwiperSlide>
-      <SwiperSlide>
-        <TestimonialsCard />
-      </SwiperSlide>
-      <SwiperSlide>
-        <TestimonialsCard />
-      </SwiperSlide>
-      <SwiperSlide>
-        <TestimonialsCard />
-      </SwiperSlide>
-      <SwiperSlide>
-        <TestimonialsCard />
-      </SwiperSlide>
+      {testimonials.map((testimonial, index) => (
+        <SwiperSlide key={index}>
+          <TestimonialsCard
+            name={testimonial.name}
+            place={testimonial.place}
+            description={testimonial.description}
+          />
+        </SwiperSlide>
+      ))}
     </Swiper>
   );
 };
